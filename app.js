99ad17313aa1ab65d69c063df5afeaa1784313f6
Cooper <dancersoftaranto@gmail.com>
2022-09-25 17:57:01 +0300

    uzun isim düzenleme, TR ekleme

--- app.js
+++ app.js
@@ -1,53 +1,55 @@
 const key = "6e1a3eda9fa53b82169bd49471c74f36";
+const input = document.getElementById("input");
+const button = document.getElementById("submit");
+const ulList = document.querySelector(".ulList");
+const explanation = document.querySelector(".explain");
+console.log(ulList);
 
-const input = document.getElementById("input")
-const button = document.getElementById("submit")
 
-const weather = ()=> {
-getResult(input.value)
-}
-
-const getResult = (city) =>{
+//? Api'den sorgu yapmak için gerekli
+const getResult = (city) => {
   let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric&lang=tr`;
 
-  fetch(api).then((res) => res.json()).then((data)=>{
-    console.log(data);
-    //! const {login, avatar_url} = data ile destruct yapılsa
-    let city = document.querySelector(".city");
-    city.innerText = `${data.name}, ${data.sys.country}`;
-    let temp = document.querySelector(".temp");
-    temp.innerText = `${Math.round(data.main.temp)}°C`;
-    let icon = document.querySelector(".icon");
-    icon.innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="">`;
-    let desc = document.querySelector(".description");
-    desc.innerText = `${(data.weather[0].description).toUpperCase()}`;
-  })
-  input.value = "" 
-}
+  fetch(api)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      const { name, main, weather, sys } = data;
+      ulList.innerHTML += `
+      <li>
+        <div class="city">${name.replace("Province", "")}
+        <span class="country">${sys.country}</span> 
+      </div>
+        <div class="temp">${Math.round(main.temp)}°C</div>
+        <div class="icon"><img src="http://openweathermap.org/img/w/${
+          weather[0].icon
+        }.png" alt=""></div>
+        <div class="description">${weather[0].description.toUpperCase()}</div>
+      </li>`;
+    });
+  input.value = "";
+};
 
 
+//? Submit butonuna basınca sorgu çalışsın
+button.addEventListener("click", () => {
+  citiesArray = [];
+  if (!input.value) {
+    explanation.innerText = "Please enter a city name"
+  } else {
+    explanation.innerText = ""
+    citiesArray.push(input.value.toLowerCase());
+    console.log(citiesArray);
+    return getResult(input.value);
+  }
+});
 
-
-button.addEventListener("click", weather)
-// {
-//     if (!input.value) {
-//     alert(`Enter a task`);
-//     }
-
-//       // input.value = "";
-// }
-
-
-
-
-
-//! Enter tuşu ile input girişi aktif olsun
+//? Enter tuşu ile input girişi aktif olsun
 input.addEventListener("keydown", (e) => {
   e.key === "Enter" && button.click();
 });
 
-
-//! açılınca ve refresh olunca input aktif olsun
+//? açılınca ve refresh olunca input aktif olsun
 window.addEventListener("load", () => {
   input.focus();
 });
